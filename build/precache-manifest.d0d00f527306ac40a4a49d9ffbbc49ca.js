37481ae006dbe3810af14b711c58b0b3e04782d5
lokesh <g.sailokesh9@gmail.com>
2022-12-31 14:38:22 +0530

    Added updated info

---
+++ build/precache-manifest.d0d00f527306ac40a4a49d9ffbbc49ca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfbb1a2afe607233c952",
+    "url": "/portfolio/static/css/main.a512cf99.chunk.css"
+  },
+  {
+    "revision": "bfbb1a2afe607233c952",
+    "url": "/portfolio/static/js/main.086064ac.chunk.js"
+  },
+  {
+    "revision": "65e7d78a5d6633851ba4",
+    "url": "/portfolio/static/js/runtime~main.095e9476.js"
+  },
+  {
+    "revision": "966c3429b1491a4accd3",
+    "url": "/portfolio/static/js/2.aa1da47d.chunk.js"
+  },
+  {
+    "revision": "efb71002a0fdbb4113c1",
+    "url": "/portfolio/static/js/3.e513e70d.chunk.js"
+  },
+  {
+    "revision": "7e4f51455e67f0f2da751ae0369fffe3",
+    "url": "/portfolio/static/media/Lokesh.7e4f5145.jpg"
+  },
+  {
+    "revision": "663a61f6559eb64219666a4d17620547",
+    "url": "/portfolio/static/media/RBC.663a61f6.png"
+  },
+  {
+    "revision": "94941ef2365dbaebad13ec028c4ff955",
+    "url": "/portfolio/static/media/Composit.94941ef2.png"
+  },
+  {
+    "revision": "85fa67b6b22c6ea844e0aeeff6940c69",
+    "url": "/portfolio/static/media/InterviewScheduler.85fa67b6.png"
+  },
+  {
+    "revision": "0ae41b59965730e6976a1dcd4c1bdfd6",
+    "url": "/portfolio/static/media/makerspace.0ae41b59.png"
+  },
+  {
+    "revision": "209677ba2b8abc7e68908efaeb61d751",
+    "url": "/portfolio/static/media/ICPicture.209677ba.jpg"
+  },
+  {
+    "revision": "e18fd3e36d29ba280e501076f831f5fe",
+    "url": "/portfolio/index.html"
+  }
+];
